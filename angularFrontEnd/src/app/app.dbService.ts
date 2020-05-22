da7e2da8388e661ade906db414d494fc43fc1f96
ehansen247 <ehansen247@gmail.com>
2020-05-22 00:35:52 -0400

    Parent directory changes

--- angularFrontEnd/src/app/app.dbService.ts
+++ angularFrontEnd/src/app/app.dbService.ts
@@ -1,18 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 declare function SHA1(msg : string) : any;
 
 @Injectable({
-    providedIn: 'root',
-  })
+  providedIn: 'root',
+})
 
-  export class DbService {
-  constructor(private http: HttpClient) { }
+export class DbService {
+  constructor(private http: HttpClient) {
+    // Should be http://localhost:8000/api/ for local
+    if (environment.production) {
+      this.apiUrl = "http://localhost:8000/api/";
+    } else {
+      this.apiUrl = "http://localhost:8000/api/";
+    }
+  }
 
-  apiUrl = "http://localhost:8000/api/";
+  apiUrl = "";
 
   getQuery(text, type) {
+    console.log("Reached!!!");
+    console.log(this.apiUrl);
     const results = this.http.post(this.apiUrl + "search", {"text": text, "type": type});
     return results;
   }
@@ -33,8 +43,3 @@ declare function SHA1(msg : string) : any;
     return results;
   }
 }
-
-// export interface Config {
-//     quote_text: string;
-//     author: string;
-// }
--- dbInit/pushToDb.py
+++ dbInit/pushToDb.py
@@ -8,10 +8,9 @@ from scrape import getAllQuotes
 
 def main():
 
-    conn = psycopg2.connect(dbname="d5q93bglraeukc",
-                            user="apfwknldnvcnwb", 
-                            password="21c5e15664d7689b039bc1c59f84f7dd3944a2073625f511ee972e789206806e",
-                            host="ec2-107-20-185-16.compute-1.amazonaws.com")
+    conn = psycopg2.connect(user="erichansen",
+                            dbname="inspire",
+                            host="localhost")
     cur = conn.cursor()
 
     # Uncomment any of the below options to create the tables or enter the user/author data
@@ -23,43 +22,32 @@ def main():
 
     # createQuoteTable(conn, cur)
 
-    # Only want to create and build quotes and authors tables once!
-    # for item in getAllQuotes():
-    #     q = item["quote"]
-    #     a = item["author"]
-    #     cur.execute("INSERT INTO quotes (quote_text, author, length) VALUES (%s, %s, %s)", (q, a, len(q)))
-
-    #     cur.execute("SELECT * from authors WHERE name=%s", (a,))
-    #     if cur.rowcount == 0:
-    #         cur.execute("INSERT INTO authors (name, num_quotes) VALUES (%s, %s)", (a, 1))
-    #     else:
-    #         num_quotes = cur.fetchone()[2]
-    #         cur.execute("UPDATE authors SET num_quotes=%s WHERE name=%s", (num_quotes + 1, a))
+    fillTables(conn, cur)
 
     # Save Changes and Close communication with database
     conn.commit()
     cur.close()
     conn.close()
 
-def createQuoteTable(conn, cur):
-    cur.execute("CREATE TABLE quotes (id serial PRIMARY KEY, quote_text text, author varchar(255), length integer);")
-    
+# Fill Quote and Author Tables with Values
+def fillTables(conn, cur):
     for item in getAllQuotes():
         q = item["quote"]
         a = item["author"]
-        cur.execute("INSERT INTO quotes (%s, %s, %s)", (q, a, len(q)))
+        cur.execute("INSERT INTO quotes (quote_text, author, length) VALUES (%s, %s, %s)", (q, a, len(q)))
+
+        cur.execute("SELECT * from authors WHERE name=%s", (a,))
+        if cur.rowcount == 0:
+            cur.execute("INSERT INTO authors (name, num_quotes) VALUES (%s, %s)", (a, 1))
+        else:
+            num_quotes = cur.fetchone()[2]
+            cur.execute("UPDATE authors SET num_quotes=%s WHERE name=%s", (num_quotes + 1, a))
+
+def createQuoteTable(conn, cur):
+    cur.execute("CREATE TABLE quotes (id serial PRIMARY KEY, quote_text text, author varchar(255), length integer);")
 
 def createAuthorTable(conn, cur):
     cur.execute("CREATE TABLE authors (id serial PRIMARY KEY, name varchar(255), num_quotes integer);")
-    
-    for item in getAllQuotes():
-        a = item["author"]
-        cur.execute("SELECT * from authors WHERE name=%s", (a,))
-        if cur.rowCount == 0:
-            cur.execute("INSERT INTO authors (%s, %s)", (a, 1))
-        else:
-            num_quotes = cur.fetchOne()[2]
-            cur.execute("UPDATE authors SET num_quotes=%s WHERE name=%s", (num_quotes + 1, a))
 
 def createUserQuotesTable(conn, cur):
     cur.execute("CREATE TABLE userquotes (id serial PRIMARY KEY, quote_text TEXT, username varchar(255), password varchar(255));")
